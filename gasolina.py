10c0ead7f458ed6bd358e8f00809f5bd656bbaff
Matheus Cantarani <128758090+Cantarani@users.noreply.github.com>
2024-02-01 01:50:02 -0300

    Update gasolina.py

--- gasolina.py
+++ gasolina.py
@@ -7,7 +7,7 @@ gasolina_df = pd.read_csv('./gasolina.csv', sep=',')
 # Gerando o gráfico
 with sns.axes_style('whitegrid'):
   grafico = sns.lineplot(data=gasolina_df, x='dia', y='venda', palette='pastel')
-  grafico.set(title='Preço médio da gasolina na cidade de São Paulo nos primeiros dias de Julho de 2021', xlabel='Dias', ylabel='Preço Médio (R$)')
+  grafico.set(title='Preço médio da gasolina na cidade de São Paulo - Julho de 2021', xlabel='Dias', ylabel='Preço Médio (R$)')
 
 # Salvando o gráfico em uma imagem
 grafico.get_figure().savefig('./gasolina.png')
